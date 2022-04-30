5b301e4102c690397da7305babc415b44f3cc2f2
wokeoke <woke.oke@gmail.com>
2022-04-30 09:50:33 +0700

    Validate Username and Password

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,21 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+
+// setup authentication so only the requst with JWT can access the dashboard
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  // mongoose validation
+  // Joi
+  // check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
   res.send('Fake Login/Register/Signup');
 };
 
