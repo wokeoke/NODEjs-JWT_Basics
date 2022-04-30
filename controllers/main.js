b41563cef523cd696250dcc17d52eb1ec585cd88
wokeoke <woke.oke@gmail.com>
2022-04-30 10:05:12 +0700

    Sign JWT

--- controllers/main.js
+++ controllers/main.js
@@ -4,6 +4,7 @@
 
 // setup authentication so only the requst with JWT can access the dashboard
 
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -16,7 +17,16 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide email and password', 400);
   }
 
-  res.send('Fake Login/Register/Signup');
+  // just for DEMO, normally provided by DB!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for DEMO, JWT_SECRET in production use long, complex and unguessable string value!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ message: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
